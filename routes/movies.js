8fb9fdd3fd54a7d02922426a381673f67a5ff23a
Teisia Park <teisia@gmail.com>
2016-01-19 16:22:17 -0700

    reviews crud routes done

--- routes/movies.js
+++ routes/movies.js
@@ -7,4 +7,32 @@ router.get('/', function (req, res, next) {
   res.render('movies/index');
 })
 
+router.get('/:movie_id/reviews', function (req, res, next) {
+  res.render('movies/reviews/show');
+})
+
+router.get('/:movie_id/reviews/new', function (req, res, next) {
+  res.render('movies/reviews/new');
+})
+
+router.post('/:movie_id/reviews', function (req, res, next) {
+  res.redirect('/movies/:movies_id/reviews');
+})
+
+router.get('/:movie_id/reviews/:id', function (req, res, next) {
+  res.render('movies/reviews/show');
+})
+
+router.get('/:movie_id/reviews/:id/edit', function (req, res, next) {
+  res.render('movies/reviews/edit');
+})
+
+router.post('/:movie_id/reviews/:id', function (req, res, next) {
+  res.redirect('/movies/:movie_id/reviews');
+})
+
+router.post('/:movie_id/reviews/:id/delete', function (req, res, next) {
+  res.redirect('/movies/:movie_id/reviews');
+})
+
 module.exports = router;
